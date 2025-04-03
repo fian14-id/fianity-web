28edd957d0998ac72f7e10489b45c1bb491fba36
fian14-id <allifian051@gmail.com>
2025-04-03 16:07:46 +0700

    changes: /blog to development mode

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,14 +1,15 @@
 import TableTexture from "@/components/utils/TableTexture"
-import BlogComponent from "./BlogComponent"
+// import BlogComponent from "./BlogComponent"
 
 const Page = async() => {
-    const response = await fetch('http://localhost:3000/api/blog')
-    const result = await response.json()
+    // const response = await fetch('https://localhost:3000/api/blog')
+    // const result = await response.json()
 
     return (
         <div className="relative overflow-hidden border-x-1 border-foreground/10">
             <TableTexture />
-            <BlogComponent api={result} />
+            {/* <BlogComponent api={result} /> */}
+            <h1 className="font-bold uppercase text-sm">Masih Tahap Development</h1>
         </div>
     )
 }

bf6616220d65ba5a1424b3a74bc159c7924bee49
fian14-id <allifian051@gmail.com>
2025-04-14 14:28:01 +0700

    fix: close section use never used

--- src/components/CloseSection.tsx
+++ src/components/CloseSection.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { use } from "react";
 import { useMouseInteraction } from "@/app/hook/useMouseInteractions";
 import { ArrowRight } from "lucide-react";
 import { useTranslations } from "next-intl";
